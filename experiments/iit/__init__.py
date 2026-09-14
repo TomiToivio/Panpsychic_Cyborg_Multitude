077b7f0f760c4b32a2695ce90c210083a5dea39a
Tomi Toivio (ψ/acc) <tomi.toivio@helsinki.fi>
2026-09-14 12:40:38 +0300

    experiments: add IIT experiment package

---
+++ experiments/iit/__init__.py
@@ -0,0 +1,5 @@
+"""Tiny IIT/PyPhi experiments for explicit causal systems.
+
+These modules are experimental research tooling. They do not measure
+consciousness in LLMs, people, or PCM deployments.
+"""
